2168c0d3a0f68660041bed4508535ba595d085c5
Tobias Casper <tobias.casper@gmail.com>
2014-10-05 01:16:22 +0200

    Update helpers.rb

--- lib/action_presenter/helpers.rb
+++ lib/action_presenter/helpers.rb
@@ -5,7 +5,7 @@ module ActionPresenter::Helpers
     options = args.extract_options!
     object = args.first
     presenter_class = options.delete(:with) do
-      raise ArgumentError, 'Neither an object nor a presenter class has been specified' if object.nil?
+      raise ArgumentError, 'Neither object nor presenter class specified' if object.nil?
       "#{object.class.name}Presenter"
     end
     presenter_class = presenter_class.to_s.constantize unless presenter_class.is_a?(Class)
